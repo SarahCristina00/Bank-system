3cdaf00647ff8b41344878401e2d9fd47d5cf98c
Lara <Lara@10.5.190.170>
2025-02-12 21:14:18 -0300

    Começa o projeto

---
+++ src/main/java/com/mycompany/systembank/SystemBank.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.systembank;
+
+/**
+ *
+ * @author Lara
+ */
+public class SystemBank {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Começa processo");
+    }
+}
