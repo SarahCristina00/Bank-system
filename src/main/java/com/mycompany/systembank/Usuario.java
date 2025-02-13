df33c4c8892a3500778b5bfbc94fcbd0c5a95f35
Wilian Santos <14809032647@estudante.ufjf.br>
2025-02-13 20:48:13 -0300

    adiciona getters, setters e validação de CPF e email

--- src/main/java/com/mycompany/systembank/Usuario.java
+++ src/main/java/com/mycompany/systembank/Usuario.java
@@ -29,7 +29,37 @@ public class Usuario {
         this.senha = senha;
     }
      
-    
+     public String getNome() { return nome; } // retorna o nome do usuario
+    public void setNome(String nome) { this.nome = nome; }// recebe o nome do usuario
+
+    public String getCpf() { return cpf; }// retorna o cpf do usuario
+    public void setCpf(String cpf) { if (validarCpf(cpf)) this.cpf = cpf; }// recebe cpf do usuario
+
+    public String getDataNascimento() { return dataNascimento; }// retorna a data  de nascimento
+    public void setDataNascimento(String dataNascimento) { this.dataNascimento = dataNascimento; }// recebe a data de nascimento
+
+    public String getTelefone() { return telefone; } // retorna telefone do usuario
+    public void setTelefone(String telefone) { this.telefone = telefone; }//recebe o telefone do usuario
+
+    public String getEmail() { return email; } // retorna email do usuario
+    public void setEmail(String email) { if (validarEmail(email)) this.email = email; }// recebe email do usuario
+
+    public String getLogin() { return login; } // retorna login do usuario
+    public void setLogin(String login) { this.login = login; } // recebe login do usuario
+
+    public int getSenha() { return senha;} // retorna senha do usuario
+    public void setSenha(int senha) { this.senha = senha; }// recebe  a senha do usuario
+
+    // Validação de CPF (formato 000.000.000-00)
+    private boolean validarCpf(String cpf) {
+        return cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}");
+    }
+
+    // Validação de email
+    private boolean validarEmail(String email) {
+        return email.matches("^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$");
+    }
+
   }
 
 
