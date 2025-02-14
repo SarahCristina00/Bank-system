86e40619b15831e3bc339884f79a5b4a5733bad7
Sarah Cristina Freitas da Silva <163673649+SarahCristina00@users.noreply.github.com>
2025-02-14 20:43:43 -0300

    Adiciona metodos get e setters para endereço.java

--- src/main/java/com/mycompany/systembank/Endereco.java
+++ src/main/java/com/mycompany/systembank/Endereco.java
@@ -26,6 +26,27 @@ class Endereco {
         this.complemento = complemento;
         this.cep = cep;
     }
+
+    public String getRua() { return rua; } 
+    public void setRua(String rua) { this.rua = rua; }
+
+    public String getBairro() { return bairro; }
+    public void setBairro(String bairro) {this.bairro = bairro;}
+
+    public String getCidade() { return cidade; }
+    public void setCidade(String cidade) { this.cidade = cidade; }
+
+    public String getEstado() { return estado; } 
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public int  getNumero() { return numero; } 
+    public void setNumero(int numero) { this.numero = numero; }
+
+    public String getComplemento() { return complemento; } 
+    public void setComplemento(String complemento) { this.complemento = complemento; } 
+
+    public int getCep() { return cep;} 
+    public void setCep(int cep) { this.cep = cep; }
     
 }
 
