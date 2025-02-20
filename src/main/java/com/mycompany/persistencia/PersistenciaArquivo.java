ba90bba18ab8de150cda7740c141e1cfcc80495e
sarah cristina <sarahsilva34567@gmail.com>
2025-02-19 22:12:47 -0300

    Cria PersistenciaArquivo, para ler, converter e salvar os dados de usuários informados

---
+++ src/main/java/com/mycompany/persistencia/PersistenciaArquivo.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.persistencia;
+
+import java.io.*;
+
+public class PersistenciaArquivo {
+
+    public static String leArquivo(String filePath) {
+        StringBuilder content = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+
+        return content.toString();
+    }
+
+    public static void salvaArquivo(String filePath, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, false))) {
+            writer.write(content);
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+    }
+}
