97fb7ad2ccdf95495f01a298e947884c623c8ae0
Lara Dias <lara.dias@estudante.ufjf.br>
2025-02-21 10:22:50 -0300

    Adiciona função de remoção de usuário

--- src/main/java/com/mycompany/interfaces/Menu.java
+++ src/main/java/com/mycompany/interfaces/Menu.java
@@ -22,9 +22,12 @@ public class Menu extends JFrame {
         //carrega os dados dos usuarios
         usuarios = persistencia.carregarDados();
         setTitle("Sistema Bancário - Menu Principal");
-        setSize(400, 300);
+        setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(3,2));
+        
+        
+        setLayout(new GridLayout(4,0,30,30));
+        setBorder(createEmptyBorder(20, 20, 20, 20));
         
         btnCriarUsuario = new JButton("Criar Usuário");
         btnRemoverUsuario = new JButton("Remover Usuário");
@@ -32,58 +35,59 @@ public class Menu extends JFrame {
         btnSair = new JButton("Sair");
         
         btnCriarUsuario.addActionListener(e -> new MenuCriarUsuario());
-        btnRemoverUsuario.addActionListener(e-> new MenuCriarUsuario());
+        btnRemoverUsuario.addActionListener(e-> new MenuRemoverUsuario());
         btnListarUsuarios.addActionListener(e -> new MenuListarUsuarios());
         btnSair.addActionListener(e -> System.exit(0));
         
         add(btnCriarUsuario);
+        add(btnRemoverUsuario);
         add(btnListarUsuarios);
         add(btnSair);
         
-        
         setVisible(true);
     }
-    
 }
+    
 
 class MenuCriarUsuario extends JFrame {
     public MenuCriarUsuario() {
         setTitle("Criar Usuário");
-        setSize(300, 400);
+        setSize(500, 500);
         setLocationRelativeTo(null);
 
-        JPanel panel = new JPanel(new GridLayout(8, 2, 5, 5));
+        JPanel painelInformacoes = new JPanel(new GridLayout(8, 2, 5, 5));
+        painelInformacoes.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         
-        JTextField nomeField = new JTextField();
-        JTextField cpfField = new JTextField();
-        JTextField dataNascimentoField = new JTextField();
-        JTextField telefoneField = new JTextField();
-        JTextField emailField = new JTextField();
-        JTextField loginField = new JTextField();
-        JPasswordField senhaField = new JPasswordField();
+        JTextField campoNome = new JTextField();
+        JTextField campoCPF = new JTextField();
+        JTextField campoDataNascimento = new JTextField();
+        JTextField campoTelefone = new JTextField();
+        JTextField campoEmail = new JTextField();
+        JTextField campoLogin = new JTextField();
+        JPasswordField campoSenha = new JPasswordField();
         JButton criarBtn = new JButton("Criar");
         
-        panel.add(new JLabel("Nome Completo:")); panel.add(nomeField);
-        panel.add(new JLabel("CPF:")); panel.add(cpfField);
-        panel.add(new JLabel("Data de Nascimento:")); panel.add(dataNascimentoField);
-        panel.add(new JLabel("Telefone:")); panel.add(telefoneField);
-        panel.add(new JLabel("Email:")); panel.add(emailField);
-        panel.add(new JLabel("Login:")); panel.add(loginField);
-        panel.add(new JLabel("Senha:")); panel.add(senhaField);
-        panel.add(criarBtn);
+        painelInformacoes.add(new JLabel("Nome Completo:")); painelInformacoes.add(campoNome);
+        painelInformacoes.add(new JLabel("CPF:")); painelInformacoes.add(campoCPF);
+        painelInformacoes.add(new JLabel("Data de Nascimento:")); painelInformacoes.add(campoDataNascimento);
+        painelInformacoes.add(new JLabel("Telefone:")); painelInformacoes.add(campoTelefone);
+        painelInformacoes.add(new JLabel("Email:")); painelInformacoes.add(campoEmail);
+        painelInformacoes.add(new JLabel("Login:")); painelInformacoes.add(campoLogin);
+        painelInformacoes.add(new JLabel("Senha:")); painelInformacoes.add(campoSenha);
+        painelInformacoes.add(criarBtn);
         
-        add(panel);
+        add(painelInformacoes);
         setVisible(true);
         
         criarBtn.addActionListener(e -> {
             Usuario novoUsuario = new Usuario(
-                nomeField.getText(),
-                cpfField.getText(),
-                dataNascimentoField.getText(),
-                telefoneField.getText(),
-                emailField.getText(),
-                loginField.getText(),
-                Integer.parseInt(new String(senhaField.getPassword()))
+                campoNome.getText(),
+                campoCPF.getText(),
+                campoDataNascimento.getText(),
+                campoTelefone.getText(),
+                campoEmail.getText(),
+                campoLogin.getText(),
+                Integer.parseInt(new String(campoSenha.getPassword()))
             );
             BankSystem.usuarios.add(novoUsuario);
             //atualiza os dados após execução
@@ -93,15 +97,48 @@ class MenuCriarUsuario extends JFrame {
         });
     }
 }
-    
+   
+class MenuRemoverUsuario extends JFrame{
+    public MenuRemoverUsuario(){
+        setTitle("Sistema Bancário - Remoção de Usuário");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+         
+        JPanel panel = new JPanel(new GridLayout(2, 2, 20, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        JTextField campoNome = new JTextField();
+        panel.add(new JLabel("Nome do Usuário a ser removido:")); panel.add(campoNome);
+       
+        JButton removerBtn = new JButton("Remover Usuário");
+         panel.add(removerBtn);
+        
+        removerBtn.addActionListener(e -> {
+                String nome = campoNome.getText();
+                
+                for(Usuario usuario : usuarios){
+                    if(usuario.getNome().equals(nome)){
+                        usuarios.remove(usuario);
+                        Menu.persistencia.salvarDados(usuarios);
+                        JOptionPane.showMessageDialog(this, "Usuário removido com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                         dispose();
+                         return;
+                    }
+                }
+                    JOptionPane.showMessageDialog(this, "Usuário não encontrado.", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+          );
+            
+        
+        add(panel);
+        setVisible(true);
+    }
+}
 
 class MenuListarUsuarios extends JFrame {
-    
-    private JList<Usuario> jlUsuarios;
-    
-    public MenuListarUsuarios() {
+        private JList<Usuario> jlUsuarios;
+        public MenuListarUsuarios() {
         setTitle("Lista de Usuários");
-        setSize(400, 300);
+        setSize(500, 500);
         setLocationRelativeTo(null);
         
         JPanel lista = new JPanel();
