8b9970d68e3ea5ba98792a28511fdf95613d5f99
Lara Dias <lara.dias@estudante.ufjf.br>
2025-02-21 17:42:49 -0300

    Adiciona metodo de edição dos usuarios

--- src/main/java/com/mycompany/interfaces/Menu.java
+++ src/main/java/com/mycompany/interfaces/Menu.java
@@ -14,7 +14,11 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Menu extends JFrame {
-    private JButton btnCriarUsuario, btnRemoverUsuario, btnListarUsuarios, btnSair;
+    private JButton botaoCriarUsuario = new JButton("Criar Usuário"),
+        botaoEditarUsuario = new JButton("Editar Usuário"),
+        botaoRemoverUsuario = new JButton("Remover Usuário"),
+        botaoListarUsuarios = new JButton("Listar Usuários"),
+        botaoSair = new JButton("Sair");
     //instancia arquivo para carregar os dados
      public static PersistenciaUsuarios persistencia = new PersistenciaUsuarios();
     
@@ -24,25 +28,24 @@ public class Menu extends JFrame {
         usuarios = persistencia.carregarDados();
         setTitle("Sistema Bancário - Menu Principal");
         setSize(500, 500);
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        JPanel painelMenu = new JPanel(new GridLayout(4,1,20,20));
-        painelMenu.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        JPanel painelMenu = new JPanel(new GridLayout(5,1,20,20));
+        painelMenu.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
         
-        btnCriarUsuario = new JButton("Criar Usuário");
-        btnRemoverUsuario = new JButton("Remover Usuário");
-        btnListarUsuarios = new JButton("Listar Usuários");
-        btnSair = new JButton("Sair");
         
-        btnCriarUsuario.addActionListener(e -> new MenuCriarUsuario());
-        btnRemoverUsuario.addActionListener(e-> new MenuRemoverUsuario());
-        btnListarUsuarios.addActionListener(e -> new MenuListarUsuarios());
-        btnSair.addActionListener(e -> System.exit(0));
+        botaoCriarUsuario.addActionListener(e -> new MenuCriarUsuario());
+        botaoRemoverUsuario.addActionListener(e-> new MenuRemoverUsuario());
+        botaoEditarUsuario.addActionListener(e-> new MenuEditarUsuario());
+        botaoListarUsuarios.addActionListener(e -> new MenuListarUsuarios());
+        botaoSair.addActionListener(e -> new Login());
         
-        painelMenu.add(btnCriarUsuario);
-        painelMenu.add(btnRemoverUsuario);
-        painelMenu.add(btnListarUsuarios);
-        painelMenu.add(btnSair);
+        painelMenu.add(botaoCriarUsuario);
+        painelMenu.add(botaoEditarUsuario);
+        painelMenu.add(botaoRemoverUsuario);
+        painelMenu.add(botaoListarUsuarios);
+        painelMenu.add(botaoSair);
         
         add(painelMenu);
         setVisible(true);
@@ -182,9 +185,6 @@ class MenuCriarUsuario extends JFrame {
                    campoCEP.getText()
                );
                
-               ContaBancaria conta = new ContaBancaria(
-                    Integer.parseInt(campoConta.getText())
-               );
                
                Cliente novoUsuario = new Cliente(
                         campoNome.getText(),
@@ -193,8 +193,7 @@ class MenuCriarUsuario extends JFrame {
                         campoTelefone.getText(),
                         campoEmail.getText(),
                        Integer.parseInt(new String(campoSenha.getPassword())),
-                        endereco,
-                         conta
+                       endereco
                     );
                 //adiciona novo usuario a lista de usuarios
                 usuarios.add(novoUsuario);    
@@ -208,7 +207,133 @@ class MenuCriarUsuario extends JFrame {
         setVisible(true);
     }
 }
-   
+
+class MenuEditarUsuario extends JFrame {
+    public MenuEditarUsuario() {
+        setTitle("Sistema Bancário - Editar Usuário");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+
+        // cria painel
+        JPanel painelInformacoes = new JPanel(new GridLayout(8, 2, 5, 5));
+
+        // campos
+        JTextField campoNome = new JTextField();
+        JTextField campoCPF = new JTextField();
+        JTextField campoDataNascimento = new JTextField();
+        JTextField campoTelefone = new JTextField();
+        JTextField campoEmail = new JTextField();
+        JPasswordField campoSenha = new JPasswordField();
+        JButton botaoEditar = new JButton("Editar");
+
+        //adicao dos campos ao painel
+        painelInformacoes.add(criarCampo("Nome Completo: ", campoNome));
+        painelInformacoes.add(criarCampo("CPF: ", campoCPF));
+        painelInformacoes.add(criarCampo("Data de Nascimento: ", campoDataNascimento));
+        painelInformacoes.add(criarCampo("Telefone: ", campoTelefone));
+        painelInformacoes.add(criarCampo("Email: ", campoEmail));
+        painelInformacoes.add(criarCampo("Senha: ", campoSenha));
+
+        // campos do cliente
+        JPanel painelCliente = new JPanel(new GridLayout(7, 2, 5, 5));
+        JTextField campoRua = new JTextField();
+        JTextField campoNumero = new JTextField();
+        JTextField campoBairro = new JTextField();
+        JTextField campoCidade = new JTextField();
+        JTextField campoEstado = new JTextField();
+        JTextField campoComplemento = new JTextField();
+        JTextField campoCEP = new JTextField();
+        JTextField campoConta = new JTextField();
+        
+        //adiciona campos do cliente ao painel
+        painelCliente.add(criarCampo("Rua: ", campoRua));
+        painelCliente.add(criarCampo("Número: ", campoNumero));
+        painelCliente.add(criarCampo("Bairro: ", campoBairro));
+        painelCliente.add(criarCampo("Cidade: ", campoCidade));
+        painelCliente.add(criarCampo("Estado: ", campoEstado));
+        painelCliente.add(criarCampo("Complemento: ", campoComplemento));
+        painelCliente.add(criarCampo("CEP: ", campoCEP));
+        painelCliente.add(criarCampo("Conta: ", campoConta));
+
+        painelCliente.setVisible(false);
+
+        // Adiciona os componentes na tela
+        JPanel painelPrincipal = new JPanel(new BorderLayout());
+        painelPrincipal.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        painelPrincipal.add(painelInformacoes, BorderLayout.CENTER);
+        painelPrincipal.add(painelCliente, BorderLayout.SOUTH);
+
+        add(painelPrincipal);
+        add(botaoEditar, BorderLayout.SOUTH);
+
+        //caixa de seleção de usuários para editar
+        JComboBox<Usuario> comboBoxUsuarios = new JComboBox<>(usuarios.toArray(new Usuario[0]));
+        add(comboBoxUsuarios, BorderLayout.NORTH);
+
+        // retorna os campos de acordo com usuario selecionado
+        comboBoxUsuarios.addActionListener(e -> {
+            Usuario usuarioSelecionado = (Usuario) comboBoxUsuarios.getSelectedItem();
+            if (usuarioSelecionado != null) {
+                campoNome.setText(usuarioSelecionado.getNome());
+                campoCPF.setText(usuarioSelecionado.getCpf());
+                campoDataNascimento.setText(usuarioSelecionado.getDataNascimento());
+                campoTelefone.setText(usuarioSelecionado.getTelefone());
+                campoEmail.setText(usuarioSelecionado.getEmail());
+                campoSenha.setText(String.valueOf(usuarioSelecionado.getSenha()));
+
+                //caso seja um Cliente
+                if(usuarioSelecionado.getTipoUsuario().equals("cliente")) {
+                    painelCliente.setVisible(true);
+                    Cliente cliente = (Cliente) usuarioSelecionado;
+                    Endereco endereco = cliente.getEndereco();
+                    campoRua.setText(endereco.getRua());
+                    campoNumero.setText(String.valueOf(cliente.getEndereco().getNumero()));
+                    campoBairro.setText(cliente.getEndereco().getBairro());
+                    campoCidade.setText(cliente.getEndereco().getCidade());
+                    campoEstado.setText(cliente.getEndereco().getEstado());
+                    campoComplemento.setText(cliente.getEndereco().getComplemento());
+                    campoCEP.setText(cliente.getEndereco().getCep());
+                     campoConta.setText(String.valueOf(cliente.getConta()));
+                }
+            }
+        });
+
+
+
+        // Evento ao clicar no botão de editar
+        botaoEditar.addActionListener(e -> {
+            Usuario usuarioSelecionado = (Usuario) comboBoxUsuarios.getSelectedItem();
+            if(usuarioSelecionado != null) {
+                // Edita os dados do usuário selecionado
+                usuarioSelecionado.setNome(campoNome.getText());
+                usuarioSelecionado.setCpf(campoCPF.getText());
+                usuarioSelecionado.setDataNascimento(campoDataNascimento.getText());
+                usuarioSelecionado.setTelefone(campoTelefone.getText());
+                usuarioSelecionado.setEmail(campoEmail.getText());
+                usuarioSelecionado.setSenha(Integer.parseInt(new String(campoSenha.getPassword())));
+
+                if(usuarioSelecionado.getTipoUsuario().equals("cliente")) {
+                    Cliente cliente = (Cliente) usuarioSelecionado;
+                    cliente.getEndereco().setRua(campoRua.getText());
+                    cliente.getEndereco().setNumero(Integer.parseInt(campoNumero.getText()));
+                    cliente.getEndereco().setBairro(campoBairro.getText());
+                    cliente.getEndereco().setCidade(campoCidade.getText());
+                    cliente.getEndereco().setEstado(campoEstado.getText());
+                    cliente.getEndereco().setComplemento(campoComplemento.getText());
+                    cliente.getEndereco().setCep(campoCEP.getText());
+                }
+
+                // att os dados após a edição
+                Menu.persistencia.salvarDados(usuarios);
+                JOptionPane.showMessageDialog(this, "Usuário editado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                setVisible(false);
+            }
+        });
+
+        setVisible(true);
+    }
+}
+ 
 class MenuRemoverUsuario extends JFrame{
     public MenuRemoverUsuario(){
         setTitle("Sistema Bancário - Remover Usuário");
