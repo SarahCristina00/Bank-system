e5f190db3efcf7def346099cf8b1aeb261966f8c
Sarah Cristina <sarahsilva34567@gmail.com>
2025-03-06 21:28:10 -0300

    Atualiza arquivos com nome dos autores

--- src/main/java/com/mycompany/persistencia/Persistencia.java
+++ src/main/java/com/mycompany/persistencia/Persistencia.java
@@ -1,3 +1,8 @@
+/**
+@author Lara da Silva Dias (202376010)
+@author Sarah Cristina (202376034)
+@author Wilian Santos (202276040)
+ */
 package com.mycompany.persistencia;
 
 import java.util.List;
